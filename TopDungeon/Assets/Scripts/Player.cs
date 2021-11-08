94970974c05bb1465320d8e9045815aa0314eaab
Marco Man <wingsun526@gmail.com>
2021-11-08 17:11:54 -0500

    Character movement and flip sprite

--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -1,19 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-    private BoxCollider2D boxCollider;
+    [SerializeField] private float moveSpeed = 1f;
+    private Vector2 rawInput;
+
+    private Rigidbody2D myRigidbody2D;
+    private BoxCollider2D myBoxCollider2D;
     void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     
     void FixedUpdate()
     {
+        FlipSprite();
+        Run();
+    }
+
+    private void FlipSprite()
+    {
+        float temp = rawInput.x;
+        if (temp > 0)
+        {
+            transform.localScale = new Vector3(rawInput.x, 1, 1);
+        }
+        else if(temp < 0)
+        {
+            transform.localScale = new Vector3(rawInput.x, 1, 1);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void Run()
+    {
+        //Vector2 delta = rawInput * (moveSpeed * Time.deltaTime);
+        myRigidbody2D.velocity = rawInput * moveSpeed;
         
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
     }
 }
