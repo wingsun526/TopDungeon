2616e3ea9714b3b822b74285b45d6bffa545a7a2
Marco Man <wingsun526@gmail.com>
2021-11-11 22:06:25 -0500

    Tile Map started

--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,50 +7,100 @@ using UnityEngine.InputSystem;
 //[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
+    
     private Vector2 rawInput;
+    private RaycastHit2D hitX;
+    private RaycastHit2D hitY;
+    //private Vector2 playerShouldMove;
 
-    private Rigidbody2D myRigidbody2D;
+   
     private BoxCollider2D myBoxCollider2D;
     void Start()
     {
-        myRigidbody2D = GetComponent<Rigidbody2D>();
         myBoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     
-    void FixedUpdate()
+    void Update()
     {
-        FlipSprite();
         Run();
+        FlipSprite();
     }
 
     private void FlipSprite()
     {
-        float temp = rawInput.x;
-        if (temp > 0)
+        bool playerHasHorizontalSpeed = Mathf.Abs(rawInput.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
         {
-            transform.localScale = new Vector3(rawInput.x, 1, 1);
-        }
-        else if(temp < 0)
-        {
-            transform.localScale = new Vector3(rawInput.x, 1, 1);
-        }
-        else
-        {
-            return;
+            // might want to change the hard coded value 1f in this statement!!
+            transform.localScale = new Vector2(Mathf.Sign(rawInput.x), 1f);
+            
         }
     }
 
     private void Run()
     {
-        //Vector2 delta = rawInput * (moveSpeed * Time.deltaTime);
-        myRigidbody2D.velocity = rawInput * moveSpeed;
-        
+        hitX = Physics2D.BoxCast(transform.position,
+            myBoxCollider2D.size,
+            0,
+            new Vector2(0, rawInput.y),
+            Mathf.Abs(rawInput.y * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking"));
+        if (hitX.collider == null)
+        {
+            transform.Translate(0, rawInput.y * Time.deltaTime, 0);
+        }
+        hitY = Physics2D.BoxCast(transform.position,
+            myBoxCollider2D.size,
+            0,
+            new Vector2(rawInput.x, 0),
+            Mathf.Abs(rawInput.x * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking"));
+        if (hitY.collider == null)
+        {
+            transform.Translate(rawInput.x * Time.deltaTime, 0, 0);
+        }
     }
-
+    
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
+        //playerShouldMove = rawInput;
+        //playerShouldMove.y = rawInput.y;
     }
+
+    /*private void Run()
+    {
+        //vector2 in a vector3 method
+        CollDetect();
+        transform.Translate(playerShouldMove * Time.deltaTime);
+        
+    }
+
+    private void CollDetect()
+    {
+        hitX = Physics2D.BoxCast(transform.position,
+            myBoxCollider2D.size,
+            0,
+            new Vector2(0, rawInput.y),
+            Mathf.Abs(rawInput.y * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking"));
+        if (hitX.collider != null)
+        {
+            playerShouldMove.y = 0;
+        }
+        hitY = Physics2D.BoxCast(transform.position,
+            myBoxCollider2D.size,
+            0,
+            new Vector2(rawInput.x, 0),
+            Mathf.Abs(rawInput.x * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking"));
+        if (hitY.collider != null)
+        {
+            playerShouldMove.x = 0;
+        }
+    }*/
+
+    
 }
