e354aa051c203f7187ba2bb77cfebbc272a3514e
Marco Man <wingsun526@gmail.com>
2021-11-15 18:55:31 -0500

    Collidable and Collectable

--- TopDungeon/Assets/Scripts/Chest.cs
+++ TopDungeon/Assets/Scripts/Chest.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
-public class Chest : MonoBehaviour
+public class Chest : Collectable
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite emptyChest;
+    public int pesosAmount = 5;
+    protected override void OnCollect()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        if(!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChest;
+            Debug.Log("Grand " + pesosAmount + " pesos!");
+        }
         
     }
 }
