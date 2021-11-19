2e222f122018e1fb05558f73608b2f3e5bbce410
Marco Man <wingsun526@gmail.com>
2021-11-18 19:49:54 -0500

    combat system

--- TopDungeon/Assets/Scripts/Mover.cs
+++ TopDungeon/Assets/Scripts/Mover.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour
+public abstract class Mover : Fighter
 {
-    // Start is called before the first frame update
-    void Start()
+    protected BoxCollider2D boxCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    protected float ySpeed = 0.75f;
+    protected float xSpeed = 1.0f;
+    
+    protected virtual void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    protected virtual void UpdateMotor(Vector3 input)
     {
+        // Reset MoveDelta
+        moveDelta = new Vector3(input.x * xSpeed, input.y * ySpeed);
         
+        // Swap sprite direction, wether you're going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        
+        // Add push vector, if any
+        moveDelta += pushDirection;
+        
+        // Reduce push force every frame, based off recovery speed
+        pushDirection = Vector3.Lerp(pushDirection, Vector3.zero, pushRecoverySpeed);
+        
+        
+        // Make sure we can move in this direction, by casting a box there first, if the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y),
+            Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask( "Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            
+            //Make this thing move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+            
+        }
+        
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0),
+            Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            
+            //Make this thing move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+            
+        }
     }
 }
